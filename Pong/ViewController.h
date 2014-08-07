0a155efd201702328d190fca14f9c8d3a8c75be0
oliverstaton <oliver.william.staton@gmail.com>
2014-08-06 22:56:30 -0400

    Initial Commit

---
+++ Pong/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pong
+//
+//  Created by Oliver Staton on 8/6/14.
+//  Copyright (c) 2014 OllieStat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Pong/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Pong
+//
+//  Created by Oliver Staton on 8/6/14.
+//  Copyright (c) 2014 OllieStat. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
